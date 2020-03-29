e8888452a671e29306cb4558ffa6b3091f2667f0
Vlad Deba <vladislavdeba@gmail.com>
2020-03-29 23:22:22 +0300

    notification refactor

--- View/Notification.swift
+++ View/Notification.swift
@@ -8,30 +8,29 @@
 
 import UIKit
 
-class Notification: UIView {
+class Notification : UIView {
 	
-	private let errorLabel: UILabel = {
+	private let label: UILabel = {
 		let label = UILabel()
 		label.font = UIFont.systemFont(ofSize: 16.5, weight: .light)
 		label.textColor = .white
 		return label
 	}()
 	
-	init(_ text: String, _ location: CGPoint) {
-		errorLabel.text = text
-		errorLabel.sizeToFit()
-		let inset = CGPoint(x: 16, y: 8)
-		super.init(frame: errorLabel.frame.insetBy(dx: -inset.x, dy: -inset.y))
-		
-		backgroundColor = Colors.red
+	init(_ labelText: String, _ position: CGPoint) {
+		label.text = labelText
+		label.sizeToFit()
+		let margin = CGPoint(x: 16, y: 8)
+		super.init(frame: label.frame.insetBy(dx: -margin.x, dy: -margin.y))
+		backgroundColor = .systemRed
 		layer.cornerRadius = frame.height/2
 		alpha = 0
 		
-		center.x = location.x
-		frame.origin.y = location.y
-		errorLabel.frame.origin.x += inset.x
-		errorLabel.frame.origin.y += inset.y
-		addSubview(errorLabel)
+		center.x = position.x
+		frame.origin.y = position.y
+		label.frame.origin.x += margin.x
+		label.frame.origin.y += margin.y
+		addSubview(label)
 	}
 	
 	func show() {
@@ -40,9 +39,9 @@ class Notification: UIView {
 			self.transform = CGAffineTransform.identity
 			self.alpha = 1
 		})
-		
-		UIView.animate(withDuration: 0.3, delay: 2, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseIn, animations: {
-			self.transform = CGAffineTransform(translationX: 0, y: 10)
+
+		UIView.animate(withDuration: 0.2, delay: 2, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveEaseIn, animations: {
+			self.transform = CGAffineTransform(translationX: 0, y: 5)
 			self.alpha = 0
 		}) { _ in
 			self.removeFromSuperview()
